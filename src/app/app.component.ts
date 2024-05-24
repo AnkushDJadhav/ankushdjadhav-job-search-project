222d2d763ed6168b398fd1b627eb109aa94af920
vinvins <vinnarasivins@gmail.com>
2024-05-24 21:01:40 +0530

    Minor Design fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,33 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.changeColor();
+  }
   title = 'ng-job-search';
+  changeColor()
+  {   
+    let element=document.getElementById('btnJob');   
+    let favElement=document.getElementById('btnFav');  
+    if(element){
+      element.style.backgroundColor ="yellowgreen";
+      if(favElement){
+        favElement.style.backgroundColor ="white";
+      }
+     
+    }
+  }
+
+  changeFavColor(){
+    let element=document.getElementById('btnFav');  
+    let jobElement=document.getElementById('btnJob');   
+    if(element){
+      element.style.backgroundColor ="yellowgreen";
+      if(jobElement)
+        {
+          jobElement.style.backgroundColor ="white";
+        }
+    }
+  }
 }
