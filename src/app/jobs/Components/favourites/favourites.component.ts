6ebb10b8a64539373217287ac5d7a144a443d205
vinvins <vinnarasivins@gmail.com>
2024-05-24 18:07:52 +0530

    Favourites Display Functionality Implemented

--- src/app/jobs/Components/favourites/favourites.component.ts
+++ src/app/jobs/Components/favourites/favourites.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { Job } from '../../Models/job';
+import { JobService } from '../../Services/job.service';
 
 @Component({
   selector: 'app-favourites',
@@ -7,6 +10,36 @@ import { Component } from '@angular/core';
   templateUrl: './favourites.component.html',
   styleUrl: './favourites.component.css'
 })
-export class FavouritesComponent {
+export class FavouritesComponent implements OnInit {
+
+  favouritesJobID :number[]=[];
+  jobList :Job[]=[];
+
+  constructor(private jobService :JobService){}
+  
+  ngOnInit(): void {
+    let savedFavorites= localStorage.getItem("myFavorites");
+  if(savedFavorites){
+    this.favouritesJobID=JSON.parse(savedFavorites);  
+  }
+
+  this.jobService.getAllJobs().subscribe(responseData=>{
+    
+    let result=responseData;
+    
+    for(let i=0; i<result.length; i++)
+      {
+       for(let j=0;j<this.favouritesJobID.length;j++)
+        {
+          if(result[i].id==this.favouritesJobID[j])
+            {
+              this.jobList.push(responseData[i]);
+            }
+        }
+      }
+    
+    });
+
+  }
 
 }
