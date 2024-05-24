6a601976bf8ae25154cd0d4c5f638284e672633f
vinvins <vinnarasivins@gmail.com>
2024-05-24 19:54:10 +0530

    Job Detail component Implementation

--- src/app/jobs/Components/job-detail/job-detail.component.ts
+++ src/app/jobs/Components/job-detail/job-detail.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { JobService } from '../../Services/job.service';
+import { JobDetail } from '../../Models/job-detail';
 
 @Component({
   selector: 'app-job-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,RouterModule],
   templateUrl: './job-detail.component.html',
   styleUrl: './job-detail.component.css'
 })
-export class JobDetailComponent {
+export class JobDetailComponent implements OnInit{
+  jobDetail :JobDetail[]=[];
 
+  constructor(private jobService :JobService){}
+
+  ngOnInit(): void {
+    this.jobService.getJobDetails(75278).subscribe(responseData=>{     
+      this.jobDetail[0]=responseData;
+      console.log(this.jobDetail[0]);
+  });
+ 
+  }
+
+  
 }
